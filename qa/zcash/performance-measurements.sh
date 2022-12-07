bdd25867cf85358474c910f2d94f8e1919850448
Marshall Gaucher <36639405+mdr0id@users.noreply.github.com>
2022-12-07 14:25:33 -0800

    Update performance-measurements.sh
    Add heaptrack to benchmarking squite to better support current and future needs of CI/CD.
    
    On linux systems, install via apt:
    '''
    apt install heaptrack
    ```
    This can be run with a debugger using `-d` with `heaptrack` invocation but please ensure the zcashd is built with debug enabled:
    ```
    CONFIGURE_FLAGS='--enable-debug' zcutil/build.sh
    ```
    Then ensure proper flags are set so the debugger can attach gracefully during a heaptrack session:
    ```
     echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
    ```
    
    To run against a live zcashd node, simply get the `PID` of zcash and track using `heaptrack -p PID`
    
    update massif targets to be heaptrack

--- qa/zcash/performance-measurements.sh
+++ qa/zcash/performance-measurements.sh
@@ -86,7 +86,7 @@ function zcashd_stop {
     wait $ZCASHD_PID
 }
 
-function zcashd_massif_start {
+function zcashd_heaptrack_start {
     case "$1" in
         sendtoaddress|loadwallet|listunspent)
             case "$2" in
@@ -97,7 +97,7 @@ function zcashd_massif_start {
                     use_200k_benchmark 1
                     ;;
                 *)
-                    echo "Bad arguments to zcashd_massif_start."
+                    echo "Bad arguments to zcashd_heaptrack_start."
                     exit 1
             esac
             ;;
@@ -106,16 +106,14 @@ function zcashd_massif_start {
             mkdir -p "$DATADIR/regtest"
             touch "$DATADIR/zcash.conf"
     esac
-    rm -f massif.out
-    valgrind --tool=massif --time-unit=ms --massif-out-file=massif.out ./src/zcashd -regtest -datadir="$DATADIR" -rpcuser=user -rpcpassword=password -rpcport=5983 -showmetrics=0 &
+    heaptrack ./src/zcashd -regtest -datadir="$DATADIR" -rpcuser=user -rpcpassword=password -rpcport=5983 -showmetrics=0 &
     ZCASHD_PID=$!
     zcash_rpc_wait_for_start
 }
 
-function zcashd_massif_stop {
+function zcashd_heaptrack_stop {
     zcash_rpc stop > /dev/null
     wait $ZCASHD_PID
-    ms_print massif.out
 }
 
 function zcashd_valgrind_start {
@@ -133,6 +131,21 @@ function zcashd_valgrind_stop {
     wait $ZCASHD_PID
     cat valgrind.out
 }
+
+function zcashd_heaptrack_start {
+    rm -rf "$DATADIR"
+    mkdir -p "$DATADIR/regtest"
+    touch "$DATADIR/zcash.conf"
+    heaptrack ./src/zcashd -regtest -datadir="$DATADIR" -rpcuser=user -rpcpassword=password -rpcport=5983 -showmetrics=0 &
+    ZCASHD_PID=$!
+    zcash_rpc_wait_for_start
+}
+
+function zcashd_heaptrack_stop {
+    zcash_rpc stop > /dev/null
+    wait $ZCASHD_PID
+}
+
 
 function extract_benchmark_data {
     if [ -f "$1.tar.xz" ]; then
@@ -258,7 +271,7 @@ case "$1" in
         zcashd_stop
         ;;
     memory)
-        zcashd_massif_start "${@:2}"
+        zcashd_heaptrack_start "${@:2}"
         case "$2" in
             sleep)
                 zcash_rpc zcbenchmark sleep 1
@@ -321,12 +334,11 @@ case "$1" in
                 zcash_rpc zcbenchmark listunspent 1
                 ;;
             *)
-                zcashd_massif_stop
+                zcashd_heaptrack_stop
                 echo "Bad arguments to memory."
                 exit 1
         esac
-        zcashd_massif_stop
-        rm -f massif.out
+        zcashd_heaptrack_stop
         ;;
     valgrind)
         zcashd_valgrind_start
@@ -405,6 +417,73 @@ case "$1" in
                 echo "Bad arguments to valgrind-tests."
                 exit 1
         esac
+        ;;
+    heaptrack)
+        zcashd_heaptrack_start
+        case "$2" in
+            sleep)
+                zcash_rpc zcbenchmark sleep 1
+                ;;
+            parameterloading)
+                zcash_rpc zcbenchmark parameterloading 1
+                ;;
+            createsaplingspend)
+                zcash_rpc zcbenchmark createsaplingspend 1
+                ;;
+            verifysaplingspend)
+                zcash_rpc zcbenchmark verifysaplingspend 1
+                ;;
+            createsaplingoutput)
+                zcash_rpc zcbenchmark createsaplingoutput 1
+                ;;
+            verifysaplingoutput)
+                zcash_rpc zcbenchmark verifysaplingoutput 1
+                ;;
+            createjoinsplit)
+                zcash_rpc_veryslow zcbenchmark createjoinsplit 1 "${@:3}"
+                ;;
+            verifyjoinsplit)
+                zcash_rpc zcbenchmark verifyjoinsplit 1 "\"$RAWJOINSPLIT\""
+                ;;
+            solveequihash)
+                zcash_rpc_veryslow zcbenchmark solveequihash 1 "${@:3}"
+                ;;
+            verifyequihash)
+                zcash_rpc zcbenchmark verifyequihash 1
+                ;;
+            trydecryptnotes)
+                zcash_rpc zcbenchmark trydecryptnotes 1 "${@:3}"
+                ;;
+            incnotewitnesses)
+                zcash_rpc zcbenchmark incnotewitnesses 1 "${@:3}"
+                ;;
+            connectblockslow)
+                extract_benchmark_data_107134
+                zcash_rpc zcbenchmark connectblockslow 1
+                ;;
+            connectblocksapling)
+                extract_benchmark_data_1723244
+                zcash_rpc zcbenchmark connectblocksapling 1
+                ;;
+            connectblockorchard)
+                extract_benchmark_data_1708048
+                zcash_rpc zcbenchmark connectblockorchard 1
+                ;;
+            sendtoaddress)
+                zcash_rpc zcbenchmark sendtoaddress 1 "${@:4}"
+                ;;
+            loadwallet)
+                # The initial load is sufficient for measurement
+                ;;
+            listunspent)
+                zcash_rpc zcbenchmark listunspent 1
+                ;;
+            *)
+                zcashd_heaptrack_stop
+                echo "Bad arguments to heaptrack."
+                exit 1
+        esac
+        zcashd_heaptrack_stop
         ;;
     *)
         echo "Invalid benchmark type."
